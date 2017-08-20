9c33aec9f167579640c19838cc83517efb91bcd1
Curtis Forristal <curtisforristal@gmail.com>
2017-08-20 17:27:22 -0600

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require("gulp"),
     autoprefixer = require("autoprefixer"),
     cssvars = require("postcss-simple-vars"),
     nested = require("postcss-nested"),
-    cssImport = require("postcss-import");
+    cssImport = require("postcss-import"),
+    browserSync = require("browser-sync").create(); //only need to use the create() method
 
 
 
@@ -30,14 +31,32 @@ gulp.task("styles", function () {
 
 
 gulp.task("watch", function() {
+    
+    browserSync.init({
+        // So the nofitication box doesn't pop up in the top right of the site when refreshing
+        notify: false,
+        // tell browserSync where the site lives
+        server: {
+            baseDir: "app"
+        }
+    });
+    
+    
     watch("./app/index.html", function() {
-        // name of task you want to run
-        gulp.start("html");
+        browserSync.reload();
     });
 
     // ** is for any other folders (current or future) under the styles folder
     // *. is for any file that has that extension
     watch("./app/assets/styles/**/*.css", function() {
-        gulp.start("styles")
+        gulp.start("cssInject")
     });
+});
+
+
+// Have browserSync auto refresh the site when the CSS is changed
+// Also tell it to complete the "styles" task as a dependency argument first
+gulp.task("cssInject", ["styles"], function() {
+    return gulp.src("./app/temp/styles/styles.css")
+        .pipe(browserSync.stream());
 });
