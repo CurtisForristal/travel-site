850b80f3c80b71b829a634e39c91ab00362d1c3b
Curtis Forristal <curtisforristal@gmail.com>
2017-08-20 15:30:02 -0600

    begin to orgainize CSS

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require("gulp"),
     postcss = require("gulp-postcss"),
     autoprefixer = require("autoprefixer"),
     cssvars = require("postcss-simple-vars"),
-    nested = require("postcss-nested");
+    nested = require("postcss-nested"),
+    cssImport = require("postcss-import");
 
 
 
@@ -23,7 +24,7 @@ gulp.task("styles", function () {
     // through a PostCSS filter (which expects an array of arguments), but we only supply one - autoprefixer
     // to a new file in temp/styles
     return gulp.src("./app/assets/styles/styles.css")
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest("./app/temp/styles"));
 });
 
